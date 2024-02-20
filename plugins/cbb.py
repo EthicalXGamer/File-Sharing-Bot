2fee4b68268ce6b9010c145d51d3b2b5cf6b24e7
EthicalXGamer <155059899+EthicalXGamer@users.noreply.github.com>
2024-02-20 16:12:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ JOIN CONTENT WALLAH 
-                         https://t.me/contentwallah01
-                         https://t.me/contentwallah01
-                         https://t.me/contentwallah01 
-            </b>",
+            text = f"<b>○ JOIN CONTENT WALLAH                         https://t.me/contentwallah01           https://t.me/contentwallah0                https://t.me/contentwallah01</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
